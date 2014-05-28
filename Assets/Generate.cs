52f2e6881973db441a0311282b681e313ec4ed7e
dpeter99 <dpeter99@gmail.com>
2014-05-28 20:10:48 +0200

    Initial Commit

---
+++ Assets/Generate.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour
+{
+
+		public int maxSpace;
+		public int maxPlatform;
+		public int novSpace;
+		public int novPlatform;
+		public bool Block;
+		
+		public GameObject space_GO;
+		
+		public float x;
+
+		// Use this for initialization
+		void Start ()
+		{		
+				for (int i = 0; i < 100; i++) {
+			
+				
+				
+						if (Random.Range (0, 100) < 50) {
+								Block = false;
+								//novPlatform += 1;
+						} else {
+								Block = true;
+								novSpace += 1;
+						}
+						if (Block) {
+								novSpace = 0;
+						}
+//						Debug.Log (space);
+						if (maxSpace == novSpace) {
+								Block = true;
+								novSpace = 0;
+						}
+						
+//					
+						x += 3.5f;
+						if (Block) {
+								Instantiate (space_GO, new Vector3 (x, 0, 0), new Quaternion (0, 0, 0, 0));
+						}
+						Debug.Log (Block);
+				}
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				
+	
+		}
+	
+	
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetAxis ("Horizontal") < 0) {
+						
+		
+				}
+		
+		
+		}
+		
+		void OnGUI ()
+		{
+		
+				if (GUILayout.Button ("1")) {
+						transform.position = transform.position + new Vector3 (3.4f, 0, 0);
+				}
+				if (GUILayout.Button ("2")) {
+						transform.position = transform.position + new Vector3 (3.4f * 2, 0, 0);
+				}
+		
+		}
+		
+}
